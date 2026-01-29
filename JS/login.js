729d8844bd6942d2e2a46bf693d965d4f32d924f
asus <demuzxz@gmail.com>
2026-01-29 13:17:43 +0530

    promobile - created  login page components with validation

---
+++ JS/login.js
@@ -0,0 +1,40 @@
+function showpass() {
+  let pwd = document.getElementById("password");
+
+  if (pwd.type === "password") {
+    pwd.type = "text";
+  } else {
+    pwd.type = "password";
+  }
+}
+
+
+function mylogin(){
+    let a = document.getElementById("myform");
+    let em = a["email"].value;
+    let pw = a["password"].value;
+
+     let gmailRegex = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
+     let pass = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+     
+    let c = 0;
+     if(gmailRegex.test(em) && em === "saravananet32@gmail.com"){
+        c++;
+     }else{
+        alert("Entered incorrect email");
+     }
+     if(pass.test(pw) && pw === "Password123" ){
+        c++;
+     }
+     else{
+        alert("Entered incorrect password");
+     }
+
+     if(c==2){
+        alert("Login Successfull");
+     }
+}
+
+
+
+
